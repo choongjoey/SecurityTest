00fd16d966fa4e1f351b87573328633a9ad33f46
Joey <joey@repulze.com>
2021-05-07 13:10:32 +0800

    Initial Commit

---
+++ SecurityTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecurityTest
+//
+//  Created by Joey Choong on 07/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
