e93f8e0ff8ae58074a602e59892f5641b88f61e6
Joey <joey@repulze.com>
2021-05-19 20:27:21 +0800

    add jb test

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'SecurityTest' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SecurityTest
+  pod 'IOSSecuritySuite'
+
+  target 'SecurityTestTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SecurityTestUITests' do
+    # Pods for testing
+  end
+
+end
--- SecurityTest/ViewController.swift
+++ SecurityTest/ViewController.swift
@@ -1,19 +1,87 @@
 //
 //  ViewController.swift
-//  SecurityTest
+//  FrameworkClientApp
 //
-//  Created by Joey Choong on 07/05/2021.
+//  Created by wregula on 23/04/2019.
+//  Copyright © 2019 wregula. All rights reserved.
 //
+//swiftlint:disable all
 
 import UIKit
+import IOSSecuritySuite
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+class RuntimeClass {
+   @objc dynamic func runtimeModifiedFunction()-> Int {
+       return 1
+   }
 }
 
+internal class ViewController: UIViewController {
+    
+    func testHookPrint() {
+        typealias MyPrint = @convention(thin) (Any..., String, String) ->Void
+        func myPrint(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+            NSLog("print has been hooked")
+        }
+        let myprint: MyPrint = myPrint
+        let myPrintPointer = unsafeBitCast(myprint, to: UnsafeMutableRawPointer.self)
+        var oldMethod: UnsafeMutableRawPointer?
+        
+        // hook
+        replaceSymbol("$ss5print_9separator10terminatoryypd_S2StF", newMethod: myPrintPointer, oldMethod: &oldMethod)
+        print("print hasn't been hooked")
+        
+        // antiHook
+        IOSSecuritySuite.denySymbolHook("$ss5print_9separator10terminatoryypd_S2StF")
+        print("print has been antiHooked")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+//        testHookPrint()
+        
+        // Runtime Check
+        let test = RuntimeClass.init()
+        test.runtimeModifiedFunction()
+        let dylds = ["UIKit"]
+        let amIRuntimeHooked = IOSSecuritySuite.amIRuntimeHooked(dyldWhiteList: dylds, detectionClass: RuntimeClass.self, selector: #selector(RuntimeClass.runtimeModifiedFunction), isClassMethod: false)
+        // MSHook Check
+        func msHookReturnFalse(takes: Int) -> Bool {
+            /// add breakpoint at here to test `IOSSecuritySuite.hasBreakpointAt`
+            return false
+        }
+        typealias FunctionType = @convention(thin) (Int) -> (Bool)
+        func getSwiftFunctionAddr(_ function: @escaping FunctionType) -> UnsafeMutableRawPointer {
+            return unsafeBitCast(function, to: UnsafeMutableRawPointer.self)
+        }
+        let funcAddr = getSwiftFunctionAddr(msHookReturnFalse)
+
+        let jailbreakStatus = IOSSecuritySuite.amIJailbrokenWithFailMessage()
+        let title = jailbreakStatus.jailbroken ? "Jailbroken" : "Jailed"
+        let message = """
+        Jailbreak: \(jailbreakStatus.failMessage),
+        Run in emulator?: \(IOSSecuritySuite.amIRunInEmulator())
+        Debugged?: \(IOSSecuritySuite.amIDebugged())
+        HasBreakpoint?: \(IOSSecuritySuite.hasBreakpointAt(funcAddr, functionSize: nil))
+        Reversed?: \(IOSSecuritySuite.amIReverseEngineered())
+        Am I MSHooked: \(IOSSecuritySuite.amIMSHooked(funcAddr))
+        Am I runtime hooked: \(amIRuntimeHooked)
+        Am I tempered with: \(IOSSecuritySuite.amITampered([.bundleID("biz.securing.FrameworkClientApp")]).result)
+        Application executable file hash value: \(IOSSecuritySuite.getMachOFileHashValue() ?? "")
+        IOSSecuritySuite executable file hash value: \(IOSSecuritySuite.getMachOFileHashValue(.custom("IOSSecuritySuite")) ?? "")
+        Am I proxied: \(IOSSecuritySuite.amIProxied())
+        """
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default))
+
+        print("FailMessage: \(message)")
+        present(alert, animated: false)
+
+        let checks = IOSSecuritySuite.amIJailbrokenWithFailedChecks()
+        print("The failed checks are: \(checks)")
+        
+#if arch(arm64)
+        print("Loaded libs: \(IOSSecuritySuite.findLoadedDylibs() ?? [])")
+#endif
+    }
+}
